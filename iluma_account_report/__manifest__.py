753d9f9b301e5166bf56f1bff06b915dcb941c71
maurice <68526635+maurice0204@users.noreply.github.com>
2024-03-24 13:35:43 -0500

    test
    test

---
+++ iluma_account_report/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ecuadorian - Payroll',
+    'author': 'OCTUPUSTECH S.A.S',
+    'maintainer': 'OCTUPUSTECH S.A.S',
+    'website': 'https://octupustech.com/',
+    'category': 'Human Resources/Payroll',
+    'icon': '/l10n_ec/static/description/icon.png',
+    'license': 'OEEL-1',
+    'version': '16.0.0.0',
+    'summary': 'Custom Payroll Module for Ecuador',
+    'description': 'This module customizes the HR Payroll for Ecuador.',
+    'depends': ['account_reports'],
+    'data': [
+        'data/aged_payable_report.xml'
+    ],
+    'auto_install': False,
+    'installable': True
+}
+
---
+++ iluma_account_report/models/__init__.py
@@ -0,0 +1,1 @@
+from . import account_aged_partnet_balance_new
